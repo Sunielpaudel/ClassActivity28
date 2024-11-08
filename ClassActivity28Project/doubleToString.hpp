9c465718aab59e4887c30fe05bbabfd1045fea65
Suniel <Suniel.paudel2047@gmail.com>
2024-11-08 12:39:30 -0800

    added ClassActivityProject28 folder.

---
+++ ClassActivity28Project/doubleToString.hpp
@@ -0,0 +1,8 @@
+#ifndef DOUBLE_TO_STRING_HPP
+#define DOUBLE_TO_STRING_HPP
+
+#include <string>
+namespace converted {
+	std::string doubleToString(double number, int precision);
+}
+#endif  DOUBLE_TO_STRING_HPP
